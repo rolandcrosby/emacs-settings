b314cde85eaaba7f3d08729d3bcf12a4bbbc5e9a
Roland Crosby <rcrosby@mediamath.com>
2015-06-15 20:05:30 -0400

    initial commit

---
+++ Preferences.el
@@ -0,0 +1,57 @@
+;; This is the Aquamacs Preferences file.
+;; Add Emacs-Lisp code here that should be executed whenever
+;; you start Aquamacs Emacs. If errors occur, Aquamacs will stop
+;; evaluating this file and print errors in the *Messags* buffer.
+;; Use this file in place of ~/.emacs (which is loaded as well.)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+;;; from purcell/emacs.d
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (package-install package)
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+(package-initialize)
+
+(dolist (x '(
+	     evil
+	     evil-surround
+	     solarized-theme
+	     powerline-evil
+	     flycheck))
+  (require-package x))
+
+(setq evil-search-module 'evil-search
+      evil-want-C-u-scroll t
+      evil-want-C-w-in-emacs-state t)
+
+(require 'evil)
+(evil-mode t)
+(require 'evil-surround)
+(global-evil-surround-mode 1)
+(require 'powerline-evil)
+
+(require 'linum)
+(setq linum-format " %d ")
+
+(setq current-theme 'solarized-dark)
+(load-theme current-theme)
+(defun solarized-toggle-theme ()
+  (interactive)
+  (if (string= current-theme 'solarized-dark)
+    (setq current-theme 'solarized-light)
+    (setq current-theme 'solarized-dark))
+  (load-theme current-theme))
+
+(tool-bar-mode 0)
+(add-hook 'after-init-hook #'global-flycheck-mode)
---
+++ customizations.el
@@ -0,0 +1,40 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-autoface-mode nil)
+ '(aquamacs-customization-version-id 307 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(custom-safe-themes
+   (quote
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(default-frame-alist
+    (quote
+     ((cursor-type . box)
+      (vertical-scroll-bars . right)
+      (internal-border-width . 0)
+      (modeline . t)
+      (fringe)
+      (mouse-color . "black")
+      (cursor-color . "#839496")
+      (background-mode . dark)
+      (tool-bar-lines . 0)
+      (menu-bar-lines . 1)
+      (right-fringe . 11)
+      (left-fringe . 3)
+      (background-color . "#002b36")
+      (foreground-color . "#839496")
+      (font . "-*-Menlo-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+      (fontsize . 0)
+      (font-backend mac-ct ns))))
+ '(ns-tool-bar-display-mode (quote both) t)
+ '(ns-tool-bar-size-mode (quote regular) t)
+ '(visual-line-mode nil t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
